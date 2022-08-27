4f391b093e740d401f9759effbd5a4d208025685
Zuddin <zuddinizzuddin14@gmail.com>
2022-08-27 09:46:11 +0700

    final commit challenge binar 2

--- app/src/main/java/com/example/challengechapter2/MainActivity.kt
+++ app/src/main/java/com/example/challengechapter2/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.challege_chapter_dua)
+        setContentView(R.layout.activity_main)
     }
 }
